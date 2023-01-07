be819873dd0cece1a743c278cdab99b1c02a1eea
Douglas Pfeiffer Serafim <douglas17_serafim@hotmail.com>
2023-01-07 18:14:43 -0300

    Separando os Testes por arquivos para melhor manu

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n=myw482#@_$i)n_bjyril+z)sq0+_zx$f#i20(9!==d2*t=p3'
+SECRET_KEY = 'django-insecure-n=myw482#@_$i)n_bjyril+z)sq0+_zx$f#i20(9!==d2*t=p3' # noqa: 501
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -94,16 +94,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa: 501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # noqa: 501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # noqa: 501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', # noqa: 501
     },
 ]
 
--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -15,4 +15,11 @@ class RecipeURLsTest(TestCase):
         # url = reverse('recipes:category', args=(1,))
         # Exemplo de mandar com a lista
         url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEquals(url, '/')
+        self.assertEquals(url, '/recipes/category/1/')
+
+    def test_recipe_detail_url_is_correct(self):
+        # Aqui enviamos os dados, conforme a URL Espera
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        # Vendo o resultado de cima, esperamos que nos retorne oque esperamos
+        # sabendo disso, comparamos para ver se a resposta é a esperada, usando equals, se for Igual, sinal que o teste passou e a URL vai funcionar. # noqa: 501
+        self.assertEquals(url, '/recipes/1/')
